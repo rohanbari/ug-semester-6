cf23e0f6620c42ee0615477416417557fc6e46a3
Rohan Bari <rohanbari4@gmail.com>
2026-03-05 23:54:34 +0530

    Mean, median, SD in Python
    Signed-off-by: Rohan Bari <rohanbari4@gmail.com>

---
+++ src/dscc-15/mms.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+data = [10, 12, 14, 15, 18]
+
+mean = np.mean(data)
+median = np.median(data)
+sd = np.std(data)
+
+print("Current dataset: ", data)
+print("Mean: ", mean)
+print("Median: ", median)
+print("Standard Deviation: ", sd)
