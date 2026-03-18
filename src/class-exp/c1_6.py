6912d84d3753736c3bc4e05431cf56420dd6af5c
Rohan Bari <rohanbari4@gmail.com>
2026-03-18 11:59:41 +0530

    Merged distinct class programs into notebooks
    Signed-off-by: Rohan Bari <rohanbari4@gmail.com>

--- src/class-exp/c1_6.py
+++
@@ -1,9 +0,0 @@
-"""Compute factorial of a non-negative integer."""
-
-num = int(input("Input a number: "))
-
-result = 1
-for i in range(2, num + 1):
-    result *= i
-
-print(f"Factorial of {num} is {result}.")
